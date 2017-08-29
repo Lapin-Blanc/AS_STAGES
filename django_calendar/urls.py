46a294009c43384051b65631dd8bc65fe1b7f575
Lapin-Blanc <fabien.toune@gmail.com>
2017-08-29 14:32:54 +0200

    added basic shedule docx generation

--- django_calendar/urls.py
+++ django_calendar/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import calendar, download_schedule
+from .views import calendar, download_schedule, download_schedule_for_student
 
 urlpatterns = [
     url(r'^docx/(?P<traineeship>[0-9]+)/$', download_schedule, name="schedule"),
+    url(r'^schedule/(?P<student>[0-9]+)/$', download_schedule_for_student, name="student_schedule"),
     url(r'^(?P<action>create|read|update|delete)/(?P<traineeship>[0-9]+)/$', calendar, name="calendar"),
 ]
--- django_calendar/views.py
+++ django_calendar/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.utils import dateparse, timezone
+from django.utils import formats, dateparse, timezone
 from .models import Period, Traineeship, Student
 from django.core.exceptions import ValidationError
 
@@ -10,6 +10,7 @@ from datetime import datetime, date
 
 from io import BytesIO
 from docx import Document
+from docx.shared import Pt
 
 def json_access_error(request):
      return  JsonResponse(
@@ -158,3 +159,66 @@ def download_schedule(request, traineeship):
     buffer.close()
     response.write(doc)
     return response 
+
+JOURS = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+
+def download_schedule_for_student(request, student, from_date=timezone.localdate()):
+    next_monday = from_date + timezone.timedelta(days=7-from_date.weekday())
+    # télécharge l'horaire d'un étudiant particulier pour la semaine suivant la date fournie ou
+    # aujourd'hui si cette date n'est pas fournie
+    student = Student.objects.get(id=student)
+    #ts = student.traineeships.filter(date_start__lte=from_date, is_closed=False)[0]
+    ts = student.traineeships.filter(is_closed=False)[0]
+    # TODO : pas de stage ouvert, plus d'un stage ouvert, étudiant n'existant pas
+
+    # Create the HttpResponse object with the appropriate docx headers.
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    response['Content-Disposition'] = 'attachment; filename="horaire %s %s.docx"' % (student.last_name,
+            student.first_name)
+
+    buffer = BytesIO()
+
+    document = Document()
+    document.styles["Title"].font.size = Pt(18)
+    document.styles["Subtitle"].font.size = Pt(16)
+
+    document.add_heading("%s %s : du %s au %s" % (
+        ts.student.first_name,
+        ts.student.last_name,
+        next_monday.strftime("%d-%m-%Y"),
+        (next_monday + timezone.timedelta(days=6)).strftime("%d-%m-%Y"),
+        )
+    ,0)
+
+    document.add_paragraph("Stage d'%s - %s" % (ts.category, ts.place,), style="Subtitle")
+
+    table = document.add_table(rows=1, cols=5)
+    table.style = 'Light Shading Accent 1'
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Jour'
+    hdr_cells[1].text = 'De'
+    hdr_cells[2].text = 'A'
+    hdr_cells[3].text = 'Périodes'
+    hdr_cells[4].text = 'Heures'
+    for x in range(7):
+        row_day = next_monday + timezone.timedelta(days=x)
+        day_periods = ts.periods.filter(start__date=row_day).order_by('start')
+        row_cells = table.add_row().cells
+        row_cells[0].text = JOURS[x]
+        num_p = 0
+        for p in day_periods :
+            num_p += 1
+            row_cells[1].text = timezone.localtime(p.start).strftime("%H:%M")
+            row_cells[2].text = timezone.localtime(p.end).strftime("%H:%M")
+            row_cells[3].text = str(p.period_duration())
+            row_cells[4].text = str(p.hour_duration())
+            if not num_p == len(day_periods):
+                row_cells = table.add_row().cells
+    document.save(buffer)
+    # Get the value of the BytesIO buffer and write it to the response.
+    doc = buffer.getvalue()
+    buffer.close()
+    response.write(doc)
+    return response 
+
+   
